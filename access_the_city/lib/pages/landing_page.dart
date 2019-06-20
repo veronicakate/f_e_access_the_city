e153392283114ebed0a6941b748ae76138e34e68
Shamila <shamk690@gmail.com>
2019-06-20 12:16:43 +0100

    added home page

---
+++ access_the_city/lib/pages/landing_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import './landing_page.dart';
+
+class LandingPage extends StatelessWidget{
+@override 
+Widget build(BuildContext context){
+  return new Material( 
+    color: Colors.greenAccent,
+    );
+}
+
+}
