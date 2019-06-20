a67a0a877494242c740990815e0923f4cfc73db0
Veronica <Ron@Veronicas-MacBook-Air.local>
2019-06-20 16:39:47 +0100

    update with new links

--- access_the_city/lib/main.dart
+++ access_the_city/lib/main.dart
@@ -1,10 +1,10 @@
-import 'package:access_the_city/pages/landing_page.dart';
+import 'package:access_the_city/pages/landingPage.dart';
 import 'package:flutter/material.dart';
 
-// this is the starting point for the application.
-void main(){
+//this is the starting point for the application.
+void main() {
   //run app takes a widget and material app widget gives us functionality
   runApp(new MaterialApp(
-    home: new LandingPage(),
+    home: LandingPage(),
   ));
 }
