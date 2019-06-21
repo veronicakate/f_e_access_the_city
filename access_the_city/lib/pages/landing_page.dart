6f842875d4878033f96d5d47c990767c2aea9616
Shamila <shamk690@gmail.com>
2019-06-21 10:31:19 +0100

    update folder structure and navigation

---
+++ access_the_city/lib/pages/landing_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:access_the_city/pages/users/landing_user_page.dart';
+import 'package:access_the_city/pages/owners/landing_owner_page.dart';
+
+
+class LandingPage extends StatelessWidget {
+  @override
+    Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Landing Page"),
+      ),
+      body: Column(children: <Widget>[
+        RaisedButton(
+          child: Text('User'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => UserLanding()),
+            );
+          },
+        ),
+        RaisedButton(
+          child: Text('Owner'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => OwnerLanding()),
+            );
+          },
+        ),
+      ]),
+    );
+  }
+}
+
+
