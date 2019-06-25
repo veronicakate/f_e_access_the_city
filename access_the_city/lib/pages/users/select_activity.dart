9a13f83db3067d92f1be7fb47fcfe1f138e5aa91
Shamila <shamk690@gmail.com>
2019-06-25 16:43:27 +0100

    pass data to activity page

--- access_the_city/lib/pages/users/select_activity.dart
+++ access_the_city/lib/pages/users/select_activity.dart
@@ -1,24 +1,72 @@
 import 'package:access_the_city/pages/users/list_of_businesses.dart';
 import 'package:flutter/material.dart';
+// import 'package:access_the_city/pages/users/select_location.dart';
 
-class SelectActivity extends StatelessWidget {
+class  SelectActivity  extends StatefulWidget {
+  final String value;
+
+
+  SelectActivity({Key key,this.value}) : super(key:key);
+  @override
+  _SelectActivityState createState() => new _SelectActivityState();
+}
+
+class _SelectActivityState extends State<SelectActivity> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+        return Scaffold(
       appBar: AppBar(
         title: Text("Select Activity"),
       ),
-      body: Center(
-        child: RaisedButton(
+      body: Center( 
+           child: RaisedButton(
           child: Text('View Options'),
           onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => BusinessList()),
-            );
-          },
+               debugPrint("${widget.value}");        // print(t.createState().locationController.text);
+
+            // Navigator.push(
+            //   context,
+            //   MaterialPageRoute(builder: (context) => BusinessList()),
+            //   );
+     }
         ),
       ),
-    );
+  );
   }
 }
+// class SelectActivity extends StatelessWidget {
+// var loc ;
+//   @override
+//   Widget build(BuildContext context) {
+//     UserDashboard t = new UserDashboard();
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Select Activity"),
+//       ),
+//       body: Center(
+//         child: RaisedButton(
+//           child: Text('View Options'),
+//           onPressed: () {
+//             debugPrint(loc);
+//                        // print(t.createState().locationController.text);
+
+//             // Navigator.push(
+//             //   context,
+//             //   MaterialPageRoute(builder: (context) => BusinessList()),
+//             // );
+//           }
+//         ),
+//       ),
+//     );
+//   }
+//     getLocation(location) async {
+//  this.loc = location;
+//          debugPrint(this.loc
+//          );
+         
+
+//  //return print('test');;
+
+//     }
+ 
+// }
