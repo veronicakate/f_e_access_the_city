8ef693333d602b57bbc44148518bfbd6f35cf990
northcoders9 <northcoders9@northcoders9s-MacBook-Pro.local>
2019-06-25 09:10:25 +0100

    finsh landing page layout

--- access_the_city/lib/pages/landing_page.dart
+++ access_the_city/lib/pages/landing_page.dart
@@ -2,22 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:access_the_city/pages/users/landing_user_page.dart';
 import 'package:access_the_city/pages/owners/landing_owner_page.dart';
 
-
 class LandingPage extends StatelessWidget {
+
   @override
-    Widget build(BuildContext context) {
+  
+  Widget build(BuildContext context) {
+
     return Scaffold(
 
-      body: Center(child: 
-      
-      Column(
+       backgroundColor: Colors.white,
+   
+   body: Center(
+     
+    
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+        
         children: <Widget>[
-        Image.asset("images/logo.png", width:70.0,height:40.0),
 
-        RaisedButton(
-        shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),
+            Container(
+              width: 300.0,
+              height: 60.0,
+              margin: EdgeInsets.all(18.0),
+              child:
+                Image.asset("images/logo.png", width:70.0,height:40.0),
+            ),
+          
+            Container(
+              width: 300.0,
+              height: 60.0,
+          
+              margin: EdgeInsets.all(18.0),
+ 
+              child: RaisedButton(
+                shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),
           child: Text('User'),
           
           //  padding: const EdgeInsets.all(18.0),
@@ -30,14 +48,21 @@ class LandingPage extends StatelessWidget {
               MaterialPageRoute(builder: (context) => UserLanding()),
             );
           },
-        ),
         
-        RaisedButton(
-        shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),
-          child: Text('Owner'),          
-          //  padding: const EdgeInsets.all(18.0),
+        ),
+            ),
+       Container(
+         width: 300.0,
+          height: 60.0,
+ margin: EdgeInsets.all(10.0),
+      child:  RaisedButton(
+        
+
+ shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),
+          child: Text('Owner'),
+            padding: const EdgeInsets.all(18.0),
            textColor: Colors.white,
-           color: Colors.deepPurple,
+          color: Colors.deepPurple,
 
           onPressed: () {
             Navigator.push(
@@ -46,12 +71,12 @@ class LandingPage extends StatelessWidget {
             );
           },
         ),
+       ),
+
       ])
-      
-      
-      ,)
+    )
+   
     );
   }
+
 }
-
-
