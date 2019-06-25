ca8533d1f9df58facb68439227f81f9d636bc41d
northcoders9 <northcoders9@northcoders9s-MacBook-Pro.local>
2019-06-25 16:43:50 +0100

    update first page of owner registration

--- access_the_city/lib/pages/owners/owner_join_1.dart
+++ access_the_city/lib/pages/owners/owner_join_1.dart
@@ -5,19 +5,126 @@ class OwnerJoin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Register as a Business Owner"),
+      resizeToAvoidBottomPadding: false,
+        appBar: AppBar(
+       
+        backgroundColor: Colors.white,
+         elevation:0.0,
+        
+    title: Row( 
+     crossAxisAlignment:CrossAxisAlignment.center,
+      mainAxisAlignment:MainAxisAlignment.end,
+    children:[
+      Image.asset("images/logo.png", width:70.0,height:40.0),
+      Container(  
+        // padding:const EdgeInsets.all(20.0),
+      )
+      ]
+    ),
+        iconTheme: IconThemeData(color: Colors.deepPurple),
+                // iconTheme: IconThemeData(color: Colors.red,size:100, opacity: 1),
+
       ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Add your business'),
-          onPressed: () {
-            Navigator.push(
+   
+      body: Column( 
+        crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container( 
+          child: Stack(children: <Widget>[ 
+            Container(padding:  EdgeInsets.fromLTRB(15.0,100.0,0.0, 0.0),
+            child: Text( 
+              'Owner', 
+              style: TextStyle( 
+                fontSize: 80.0, fontWeight: FontWeight.normal ),
+            ),
+            )   ,
+              Container( 
+                padding:  EdgeInsets.fromLTRB(15.0,175.0,0.0, 0.0),
+            child: Text( 
+              'Register', 
+              style: TextStyle( 
+                fontSize: 80.0, fontWeight: FontWeight.normal ),
+            ),
+            ),
+            Container( 
+                padding:  EdgeInsets.fromLTRB(220.0,175.0,0.0, 0.0),
+            child: Text( 
+              '.', 
+              style: TextStyle( 
+                fontSize: 80.0, fontWeight: FontWeight.bold,color: Colors.black ),
+            ),
+            )
+          ],
+          )
+          ),
+          Container(  
+            padding: EdgeInsets.only(top:35.0,left: 20.0,right:20.0),
+            child: Column(children: <Widget>[
+              TextField( 
+                decoration:  InputDecoration( 
+                  labelText: 'EMAIL', 
+                  labelStyle: TextStyle(fontFamily: 'Montserrat', 
+                  fontWeight: FontWeight.bold , 
+                  color:Colors.black,
+                  fontSize: 20.0
+                  ),
+                focusedBorder: UnderlineInputBorder(borderSide:  BorderSide(color: Colors.black))
+
+                ),
+              ),
+              SizedBox(height: 20.0),
+              TextField( 
+                decoration:  InputDecoration( 
+                  labelText: 'PASSWORD', 
+                  labelStyle: TextStyle(fontFamily: 'Montserrat', 
+                  fontWeight: FontWeight.bold , 
+                  color:Colors.black,
+                  fontSize: 20.0
+                  ),
+                focusedBorder: UnderlineInputBorder(borderSide:  BorderSide(color: Colors.black))
+
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 20.0),
+              SizedBox(height: 40.0),
+              Container( 
+                height: 40.0, 
+                child: Material( 
+                  borderRadius:  BorderRadius.circular(20.0), 
+                  shadowColor: Colors.grey,
+                  color: Colors.grey,
+                  elevation: 7.0, 
+                  child:GestureDetector ( 
+                    onTap: () { 
+       Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => RegisterBusiness()),
-            );
-          },
-        ),
+        MaterialPageRoute(builder: (context) => RegisterBusiness()),
+          );
+                    },
+                    child:Center( 
+                      child:Text( 
+                        'NEXT', 
+                        style: TextStyle( 
+                        color: Colors.white, 
+                        fontWeight: FontWeight.bold, 
+                        fontFamily:  'Montserrat'
+                        ),
+                      ) ,
+                    )
+                  ),
+
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Container( 
+                height: 40.0,
+              )
+
+            ],
+            ),
+          )
+      ],
       ),
     );
   }
