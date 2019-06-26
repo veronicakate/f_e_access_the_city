6868910e8e7651bf6cfc9161fa094d90443eddd5
Marco F. Iannuzzi <iannuzzimf@gmail.com>
2019-06-26 14:28:28 +0100

    add main fields to business page

--- access_the_city/lib/pages/users/single_business.dart
+++ access_the_city/lib/pages/users/single_business.dart
@@ -19,13 +19,92 @@ class SingleBusiness extends StatelessWidget {
               ]),
           iconTheme: IconThemeData(color: Colors.deepPurple),
         ),
-        body: Center(
-            child: Column(
-          children: <Widget>[
-            Text('NQ Pizza',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50.0)),
-          ],
-        )));
+        body: Column(children: [
+          Center(
+              child: Text('NQ Pizza',
+                  style:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 50.0))),
+          Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                  margin: EdgeInsets.only(top: 20.0, left: 20.0),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text('Description',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20.0)),
+                        Text(
+                            'We love to party all the time at NQ Pizza\'s. Grab a Frozen Margarita and a slice of Pizza and watch the party unfold')
+                      ]))),
+          Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                  margin: EdgeInsets.only(top: 20.0, left: 20.0),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text('Rating',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20.0)),
+                        Text('Stars')
+                      ]))),
+          Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                  margin: EdgeInsets.only(top: 20.0, left: 20.0),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text('Facilities',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20.0)),
+                        Text('Stars')
+                      ]))),
+          Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                  margin: EdgeInsets.only(top: 20.0, left: 20.0),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text('Address',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20.0)),
+                        Text('Stars')
+                      ]))),
+          Center(
+            child: Column(children: [
+              Container(
+                width: 300.0,
+                height: 60.0,
+                margin: EdgeInsets.all(18.0),
+                child: RaisedButton(
+                  shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(20.0)),
+                  child: Text('Call'),
+                  //  padding: const EdgeInsets.all(18.0),
+                  textColor: Colors.white,
+                  color: Colors.deepPurple,
+                  onPressed: () {},
+                ),
+              ),
+              Container(
+                width: 300.0,
+                height: 60.0,
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(20.0)),
+                  child: Text('Get Directions'),
+                  //  padding: const EdgeInsets.all(18.0),
+                  textColor: Colors.white,
+                  color: Colors.deepPurple,
+                  onPressed: () {},
+                ),
+              )
+            ]),
+          )
+        ]));
   }
 }
