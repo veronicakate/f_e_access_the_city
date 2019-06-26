99b4ef35b6cbb089bf8e8c7c3aca2ce70c47fb09
Marco F. Iannuzzi <iannuzzimf@gmail.com>
2019-06-26 12:31:16 +0100

    add title to single business page

--- access_the_city/lib/pages/users/single_business.dart
+++ access_the_city/lib/pages/users/single_business.dart
@@ -5,20 +5,27 @@ class SingleBusiness extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Single Business"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Search Again'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => UserDashboard()),
-            );
-          },
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          title: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Image.asset("images/logo.png", width: 70.0, height: 40.0),
+                Container(
+                    // padding:const EdgeInsets.all(20.0),
+                    )
+              ]),
+          iconTheme: IconThemeData(color: Colors.deepPurple),
         ),
-      ),
-    );
+        body: Center(
+            child: Column(
+          children: <Widget>[
+            Text('NQ Pizza',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50.0)),
+          ],
+        )));
   }
 }
