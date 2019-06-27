0f5c43b57f827bef0d072b6c9eeb61a7dc4c4b5f
Marco F. Iannuzzi <iannuzzimf@gmail.com>
2019-06-27 15:00:52 +0100

    add user registration page

--- access_the_city/lib/pages/users/user_join.dart
+++ access_the_city/lib/pages/users/user_join.dart
@@ -1,24 +1,175 @@
-import 'package:access_the_city/pages/users/user_requirements.dart';
 import 'package:flutter/material.dart';
+import 'package:access_the_city/pages/users/user_requirements.dart';
+import 'package:http/http.dart' as http;
 
-class UserJoin extends StatelessWidget {
+import 'dart:convert';
+
+class UserJoin extends StatefulWidget {
+  @override
+  _UserJoinState createState() => _UserJoinState();
+}
+
+class _UserJoinState extends State<UserJoin> {
+  Map data;
+  List userData;
+  TextEditingController emailController = new TextEditingController();
+  TextEditingController passwordController = new TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       appBar: AppBar(
-        title: Text("User Join"),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        title: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Image.asset("images/logo.png", width: 70.0, height: 40.0)
+            ]),
+        iconTheme: IconThemeData(color: Colors.deepPurple),
       ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Requirements'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => UserRequirements()),
-            );
-          },
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+              child: Stack(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.fromLTRB(15.0, 70.0, 0.0, 0.0),
+                child: Text(
+                  'Register',
+                  style:
+                      TextStyle(fontSize: 80.0, fontWeight: FontWeight.normal),
+                ),
+              ),
+            ],
+          )),
+          Container(
+            padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                      labelText: 'ENTER EMAIL',
+                      labelStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 20.0),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black))),
+                ),
+                SizedBox(height: 20.0),
+                TextField(
+                    decoration: InputDecoration(
+                        labelText: 'ENTER PASSWORD',
+                        labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 20.0),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black))),
+                    obscureText: true,
+                    controller: passwordController),
+                SizedBox(height: 20.0),
+                SizedBox(height: 40.0),
+                Container(
+                  height: 60.0,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20.0),
+                    shadowColor: Colors.grey,
+                    color: Colors.grey,
+                    elevation: 7.0,
+                    child: RaisedButton(
+                        textColor: Colors.white,
+                        color: Colors.deepPurple,
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(20.0)),
+                        onPressed: () {
+                          getData(
+                              emailController.text, passwordController.text);
+                        },
+                        child: Center(
+                          child: Text(
+                            'SUBMIT',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Montserrat',
+                              fontSize: 50.0,
+                            ),
+                          ),
+                        )),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Container(
+                  height: 40.0,
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
+  }
+
+  getData(email, password) async {
+    final Map<String, String> data = {
+      "email": "$email",
+      "password": "$password"
+    };
+
+    // final Map<String, String> data = {"email":"velit.justo.nec@risusDonecegestas.co.uk","password":"WKS86ILH1XD"};
+
+    var response = await http.post(
+        'https://access-the-city-backend.herokuapp.com/api/users',
+        headers: {
+          "Accept": "application/json",
+        },
+        body: json.encode(data));
+
+    print(response.statusCode);
+    if (response.statusCode == 201) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => UserRequirements()),
+      );
+      debugPrint(response.body);
+    } else {
+      var alerDialog = AlertDialog(
+        title: Text('Dialog'),
+        content: Text('User not found'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Ok'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          debugPrint(response.body);
+          print(emailController.text);
+
+          return alerDialog;
+        },
+      );
+    }
+    emailController.text = '';
+    passwordController.text = '';
   }
 }
